bf4fb7757692a4f3f90d557781ac1671bd478664
Shakeel <shakeel@email.com>
2024-09-09 18:07:28 +0500

    update3

--- countdown.py
+++ countdown.py
@@ -1,7 +1,7 @@
 import time
 
 try:
-    user_input = int(input("Enter time (in seconds): "))
+    user_input = 10 #int(input("Enter time (in seconds): "))
     if user_input <= 0:
         print("Value must be posotive.")
     else:
@@ -9,7 +9,7 @@ try:
         while user_input >= 0:
             minutes = user_input // 60
             seconds = user_input % 60
-            print(f"Remaining time: {minutes:02d} : {seconds:02d}", end="\r")
+            print(f"Remaining time: {minutes:02d} : {seconds:02d}")
             time.sleep(1)
             user_input -= 1
         print("Countdown Finished...!")
